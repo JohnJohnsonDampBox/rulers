38943456d8da525fb09a0588332370ba910142c5
John Johnson <luckstedcraig@gmail.com>
2014-02-20 15:04:40 -0800

    removed best quotes

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -6,8 +6,6 @@ module Rulers
     def call(env)
       if env['PATH_INFO'] == '/favicon.ico'
         return [404, {'Content-Type' => 'text/html'}, []]
-      elsif env['PATH_INFO'] == '/'
-        return [200, {'Content-Type' => 'text/html'}, []]
       end
       klass, act = get_controller_and_action(env)
       controller = klass.new(env)
